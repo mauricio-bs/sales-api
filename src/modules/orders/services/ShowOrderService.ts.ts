6ba6206798a21c2a552fcfcda7741bd4213fe1bd
mauricio-bs <mauricio.3g2014@gmail.com>
2022-04-28 15:08:34 -0300

    Add orders services

---
+++ src/modules/orders/services/ShowOrderService.ts.ts
@@ -0,0 +1,15 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Order from '../typeorm/entities/Order';
+import OrdersRepository from '../typeorm/repositories/OrdersRepository';
+
+export default class ShowOrderService {
+	public async execute(id: string): Promise<Order> {
+		const ordersRepository = getCustomRepository(OrdersRepository);
+
+		const order = await ordersRepository.findById(id);
+		if (!order) throw new AppError('Order not found');
+
+		return order;
+	}
+}
