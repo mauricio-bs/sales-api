67917d0717a3ff369db4c1234b9d7cf8c9315462
mauricio-bs <mauricio.3g2014@gmail.com>
2022-05-06 18:08:22 -0300

    feat: add configurations to use s3

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,20 +2,41 @@ import path from 'path';
 import multer from 'multer';
 import crypto from 'crypto';
 
+interface IUploadConfig {
+	driver: 's3' | 'disk';
+	tmpFolder: string;
+	directory: string;
+	multer: {
+		storage: multer.StorageEngine;
+	};
+	config: {
+		aws: {
+			bucket: string;
+		};
+	};
+}
+
 const uploadFolder = path.resolve(__dirname, '..', '..', 'uploads');
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
 export default {
 	directory: uploadFolder,
 	tmpFolder,
-	storage: multer.diskStorage({
-		destination: uploadFolder,
-		filename(req, file, callback) {
-			const fileHash = crypto.randomBytes(10).toString('hex');
+	multer: {
+		storage: multer.diskStorage({
+			destination: uploadFolder,
+			filename(req, file, callback) {
+				const fileHash = crypto.randomBytes(10).toString('hex');
 
-			const filename = `${fileHash}-${file.originalname}`;
+				const filename = `${fileHash}-${file.originalname}`;
 
-			callback(null, filename);
+				callback(null, filename);
+			},
+		}),
+	},
+	config: {
+		aws: {
+			bucket: 'udemy-sales-api',
 		},
-	}),
-};
+	},
+} as IUploadConfig;
