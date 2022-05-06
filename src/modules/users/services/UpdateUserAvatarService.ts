2d8b2ec575617f00c8578c62faaa81a65c33715f
mauricio-bs <mauricio.3g2014@gmail.com>
2022-05-06 17:40:32 -0300

    feat: update avatar now use local storage provider to upload and delete files

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -5,6 +5,7 @@ import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 import uploadConfig from '@config/upload';
+import DiskStorageProvider from '@shared/providers/StorageProvider/DiskStorageProvider';
 
 interface IRequest {
 	user_id: string;
@@ -14,18 +15,18 @@ interface IRequest {
 export default class UpdateUserAvatarService {
 	public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
 		const usersRepository = getCustomRepository(UsersRepository);
+		const storageProvider = new DiskStorageProvider();
 
 		const user = await usersRepository.findById(user_id);
 		if (!user) throw new AppError('User not found');
 
 		if (user.avatar) {
-			const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-			// Verify if file really exists and delete it
-			const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-			if (userAvatarFileExists) await fs.promises.unlink(userAvatarFilePath);
+			await storageProvider.deleteFile(user.avatar);
 		}
 
-		user.avatar = avatarFilename;
+		const filename = await storageProvider.saveFile(avatarFilename);
+
+		user.avatar = filename;
 
 		await usersRepository.save(user);
 
