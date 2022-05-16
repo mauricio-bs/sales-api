294fa1607101b475b912104d265da914d936fd59
mauricio-bs <mauricio.3g2014@gmail.com>
2022-05-16 11:40:32 -0300

    feat: add interface to update only products quantities

---
+++ src/modules/products/domain/models/IUpdateProductQuantity.ts
@@ -0,0 +1,4 @@
+export interface IUpdateProductQuantity {
+	id: string;
+	quantity: number;
+}
--- src/modules/products/domain/repositories/IProductsRepository.ts
+++ src/modules/products/domain/repositories/IProductsRepository.ts
@@ -1,6 +1,7 @@
 import { IProduct } from '../models/IProduct';
 import { ICreateProduct } from '../models/ICreateProducts';
 import { IFindManyProductsByIds } from '../models/IFindManyProductsByIds';
+import { IUpdateProductQuantity } from '../models/IUpdateProductQuantity';
 
 export interface IProductsRepository {
 	findById(id: string): Promise<IProduct | undefined>;
@@ -9,6 +10,6 @@ export interface IProductsRepository {
 	findAllByIds(products: IFindManyProductsByIds[]): Promise<IProduct[]>;
 
 	create(product: ICreateProduct): Promise<IProduct>;
-	save(product: ICreateProduct): Promise<IProduct>;
+	save(product: ICreateProduct | IUpdateProductQuantity[]): Promise<IProduct>;
 	remove(product: IProduct): Promise<void>;
 }
