46f1e77c9fab5c73ca8d87ae334380a7d9f669eb
mauricio-bs <mauricio.3g2014@gmail.com>
2022-05-16 10:59:56 -0300

    feat: add users and userTokens repositories to dependency injection

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -5,6 +5,10 @@ import CustomersRepository from '@modules/customers/infra/typeorm/repositories/C
 
 import { IProductsRepository } from '@modules/products/domain/repositories/IProductsRepository';
 import ProductsRepository from '@modules/products/infra/typeorm/repositories/ProductsRepository';
+import { IUsersRepository } from '@modules/users/domain/repositories/IUsersRepository';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import { IUserTokensRepository } from '@modules/users/domain/repositories/IUSerTokensRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 
 container.registerSingleton<ICustomersRepository>(
 	'CustomersRepository',
@@ -14,4 +18,14 @@ container.registerSingleton<ICustomersRepository>(
 container.registerSingleton<IProductsRepository>(
 	'ProductsRepository',
 	ProductsRepository,
+);
+
+container.registerSingleton<IUsersRepository>(
+	'UsersRepository',
+	UsersRepository,
+);
+
+container.registerSingleton<IUserTokensRepository>(
+	'UserTokensRepository',
+	UserTokensRepository,
 );
