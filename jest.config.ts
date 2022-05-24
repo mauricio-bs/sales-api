06299d55382f522192169b6219f4fbe8e69abc6e
mauricio-bs <mauricio.3g2014@gmail.com>
2022-05-24 12:49:29 -0300

    feat: install and configure jest

--- jest.config.ts
+++ jest.config.ts
@@ -20,13 +20,13 @@ export default {
 	clearMocks: true,
 
 	// Indicates whether the coverage information should be collected while executing the test
-	// collectCoverage: false,
+	collectCoverage: true,
 
 	// An array of glob patterns indicating a set of files for which coverage information should be collected
-	// collectCoverageFrom: undefined,
+	collectCoverageFrom: ['<rootDir>/src/modules/**/services/*.ts'],
 
 	// The directory where Jest should output its coverage files
-	// coverageDirectory: undefined,
+	coverageDirectory: 'coverage',
 
 	// An array of regexp pattern strings used to skip coverage collection
 	// coveragePathIgnorePatterns: [
@@ -37,12 +37,7 @@ export default {
 	coverageProvider: 'v8',
 
 	// A list of reporter names that Jest uses when writing coverage reports
-	// coverageReporters: [
-	//   "json",
-	//   "text",
-	//   "lcov",
-	//   "clover"
-	// ],
+	coverageReporters: ['text-summary', 'lcov'],
 
 	// An object that configures minimum threshold enforcement for coverage results
 	// coverageThreshold: undefined,
@@ -126,7 +121,7 @@ export default {
 	// restoreMocks: false,
 
 	// The root directory that Jest should scan for tests and modules within
-	// rootDir: undefined,
+	rootDir: './',
 
 	// A list of paths to directories that Jest should use to search for files in
 	// roots: [
