1e34a58ab0f32ec8680525dcbb018093a9728a98
mauricio-bs <mauricio.3g2014@gmail.com>
2022-05-24 12:48:22 -0300

    refactor: create a provider to passwords hash

--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -1,4 +1,3 @@
-import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import authConfig from '@config/auth';
 import { inject, injectable } from 'tsyringe';
@@ -6,11 +5,13 @@ import AppError from '@shared/errors/AppError';
 import { ICreateSesstionRequest } from '../domain/models/ICreateSessionRequest';
 import { IUsersRepository } from '../domain/repositories/IUsersRepository';
 import { ICreateSession } from '../domain/models/ICreateSession';
+import { IHashProvider } from '../providers/HashProvider/models/IHashProvider';
 
 @injectable()
 export default class CreateSessionsService {
 	constructor(
 		@inject('UsersRepository') private usersRepository: IUsersRepository,
+		@inject('HashContainer') private hashProvider: IHashProvider,
 	) {}
 
 	public async execute({
@@ -22,7 +23,7 @@ export default class CreateSessionsService {
 			throw new AppError('Incorrect email / password combination', 401);
 		}
 
-		if (!(await compare(password, user.password))) {
+		if (!(await this.hashProvider.compareHash(password, user.password))) {
 			throw new AppError('Incorrect email / password combination', 401);
 		}
 
--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,14 +1,15 @@
-import { hash } from 'bcryptjs';
 import { inject, injectable } from 'tsyringe';
 import { IUser } from '../domain/models/IUser';
 import AppError from '@shared/errors/AppError';
 import { ICreateUser } from '../domain/models/ICreateUser';
 import { IUsersRepository } from '../domain/repositories/IUsersRepository';
+import { IHashProvider } from '../providers/HashProvider/models/IHashProvider';
 
 @injectable()
 export default class CreateUserService {
 	constructor(
 		@inject('UsersRepository') private usersRepository: IUsersRepository,
+		@inject('HashContainer') private hashProvider: IHashProvider,
 	) {}
 
 	public async execute({
@@ -19,7 +20,7 @@ export default class CreateUserService {
 		const emailExists = await this.usersRepository.findByEmail(email);
 		if (emailExists) throw new AppError('Email address already used');
 
-		const hashedPassword = await hash(password, 8);
+		const hashedPassword = await this.hashProvider.generateHash(password);
 
 		const user = this.usersRepository.create({
 			name,
