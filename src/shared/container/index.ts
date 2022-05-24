004adca72771177edc1e224795a737d471e452e8
mauricio-bs <mauricio.3g2014@gmail.com>
2022-05-24 12:52:47 -0300

    feat: add users providers importation

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,7 +2,6 @@ import { container } from 'tsyringe';
 
 import { ICustomersRepository } from '@modules/customers/domain/repositories/ICustomersRepository';
 import CustomersRepository from '@modules/customers/infra/typeorm/repositories/CustomersRepository';
-
 import { IProductsRepository } from '@modules/products/domain/repositories/IProductsRepository';
 import ProductsRepository from '@modules/products/infra/typeorm/repositories/ProductsRepository';
 import { IUsersRepository } from '@modules/users/domain/repositories/IUsersRepository';
@@ -11,6 +10,8 @@ import { IUserTokensRepository } from '@modules/users/domain/repositories/IUSerT
 import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 import { IOrdersRepository } from '@modules/orders/domain/repositories/IOrdersRepository';
 import OrdersRepository from '@modules/orders/infra/typeorm/repositories/OrdersRepository';
+
+import '@modules/users/providers';
 
 container.registerSingleton<ICustomersRepository>(
 	'CustomersRepository',
